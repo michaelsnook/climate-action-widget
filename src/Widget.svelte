4d668d64d8c6307c7a16c85c85450c03447803d6
Michael Snook <me@michaelsnook.com>
2021-08-10 03:54:56 +0530

    Explicitly scope alll widget styles

--- src/Widget.svelte
+++ src/Widget.svelte
@@ -1,18 +1,18 @@
 <script>
   import CountryPicker from './CountryPicker.svelte'
-  export let noStyles = false
-  export let noLayout = false
+  export let noStyles
+  export let noLayout
   export let theme = {}
-  let mainColor = theme.color ?? '#663399'
-  let darkColor = theme.darkColor ?? '#332e3c'
-  let actionColor = theme.actionColor ?? '#bc4747'
-  let coolColor = theme.coolColor ?? '#297373'
-  let brightColor = theme.brightColor ?? '#ffe66d'
+  let mainColor = theme.mainColor || '#663399'
+  let darkColor = theme.darkColor || '#332e3c'
+  let actionColor = theme.actionColor || '#bc4747'
+  let coolColor = theme.coolColor || '#297373'
+  let brightColor = theme.brightColor || '#ffe66d'
 
   let view = 'initial'
 </script>
 
-<section class="widget-inner" style="
+<section class="cop-widget-inner" style="
   --main-color: {mainColor};
   --dark-color: {darkColor};
   --action-color: {actionColor};
@@ -28,49 +28,55 @@
       <a href class="button button-clear" on:click|preventDefault={() => view = 'organizer'}>Organizers, get in touch</a>
     {:else if view === 'join-group'}
       <CountryPicker type="join-group" />
-      <a href on:click|preventDefault={() => view = 'initial'}>go back</a>
+      <a href on:click|preventDefault={() => view = 'initial'}>&#171; go back</a>
     {:else if view === 'find-action'}
       <CountryPicker type="find-action" />
-      <a href on:click|preventDefault={() => view = 'initial'}>go back</a>
+      <a href on:click|preventDefault={() => view = 'initial'}>&#171; go back</a>
     {:else if view === 'organizer'}
       <p>cool just email us or sthg</p>
-      <a href on:click|preventDefault={() => view = 'initial'}>go back</a>
+      <a href on:click|preventDefault={() => view = 'initial'}>&#171; go back</a>
     {/if}
   </main>
 
 </section>
 
-<style global>
-  section.widget-inner {
+{#if !noLayout}
+<style>
+  section.cop-widget-inner {
     box-sizing: border-box;
     font-size: 16px;
-    font-family: sans-serif;
     max-width: 400px;
+    min-height: 300px;
     margin: 0 auto;
   }
-  *, *:before, *:after {
+  .cop-widget-inner *,
+  .cop-widget-inner *:before,
+  .cop-widget-inner *:after {
     box-sizing: inherit;
   }
-</style>
-
-{#if !noLayout}
-<style global>
-  section.widget-inner {
-    min-height: 300px;
-  }
-  h1, h2, h3, h4, h5, h6, p, ol, ul {
+  .cop-widget-inner h1,
+  .cop-widget-inner h2,
+  .cop-widget-inner h3,
+  .cop-widget-inner h4,
+  .cop-widget-inner h5,
+  .cop-widget-inner h6,
+  .cop-widget-inner p,
+  .cop-widget-inner ol,
+  .cop-widget-inner ul {
     margin: 0;
     padding: 0;
   }
-  ol, ul {
+  .cop-widget-inner ol, .cop-widget-inner ul {
     list-style: none;
     padding-left: 0.5rem;
   }
-  .pad {
+  .cop-widget-inner .pad {
     padding: 1rem 1.6rem;
     margin-bottom: 2rem;
   }
-  select, option, input {
+  .cop-widget-inner select,
+  .cop-widget-inner option,
+  .cop-widget-inner input {
     width: 100%;
     border: 1px gray solid;
     padding: 1rem;
@@ -79,50 +85,61 @@
     display: block;
     font-size: 16px;
   }
-  a.button {
+  .cop-widget-inner a.button {
     width: 100%;
     text-align: center;
     margin: 0.3rem auto;
     display: inline-block;
   }
-  label {
+  .cop-widget-inner label {
     display: block;
     padding: 0.5rem 0;
   }
-  main.interactions {
+  .cop-widget-inner main.interactions {
     padding: 2rem 0;
-
   }
 </style>
 {/if}
 
 {#if !noStyles}
-<style global>
-	h1, h2, h3, h4, h5, h6, p, ol, ul {
+<style>
+	.cop-widget-inner h1,
+  .cop-widget-inner h2,
+  .cop-widget-inner h3,
+  .cop-widget-inner h4,
+  .cop-widget-inner h5,
+  .cop-widget-inner h6,
+  .cop-widget-inner p,
+  .cop-widget-inner ol,
+  .cop-widget-inner ul {
     font-weight: normal;
     color: var(--dark-color);
   }
-  h1 {
+  .cop-widget-inner {
+    font-family: sans-serif;
+  }
+  .cop-widget-inner h1 {
     color: var(--main-color);
     font-weight: bold;
     font-size: 3rem;
   }
-  a {
+  .cop-widget-inner a {
     color: var(--main-color);
   }
-  a.button-solid {
+  .cop-widget-inner a.button-solid {
     background-color: var(--main-color);
     color: white;
   }
-  a.button-outline {
+  .cop-widget-inner a.button-outline {
     border: 1px var(--main-color) solid;
   }
-  a.button-solid, a.button-outline {
+  .cop-widget-inner a.button-solid,
+  .cop-widget-inner a.button-outline {
     padding: 1rem 3rem;
     border-radius: 3px;
     text-decoration: none;
   }
-  a:hover {
+  .cop-widget-inner a:hover {
     text-decoration: underline;
   }
 </style>
