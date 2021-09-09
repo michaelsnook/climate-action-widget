7010787cd4c09f823c7bf9d5252bf035779e29e0
Michael Snook <me@michaelsnook.com>
2021-09-09 16:16:54 +0530

    Remove semicolons

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,13 @@
 import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n'
 
-import en from './locales/en.json';
-import enUS from './locales/en-US.json';
-import enGB from './locales/en-GB.json';
+import en from './locales/en'
+import enUS from './locales/en-US'
+import enGB from './locales/en-GB'
 
 export default function localeSetup(locale = getLocaleFromNavigator()) {
-  addMessages('en', en);
-  addMessages('en-US', enUS);
-  addMessages('en-GB', enGB);
+  addMessages('en', en)
+  addMessages('en-US', enUS)
+  addMessages('en-GB', enGB)
   init({
     fallbackLocale: 'en',
     initialLocale: locale,
